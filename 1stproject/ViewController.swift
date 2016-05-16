d883cf3130ceca221da62b6a2b3c21e1b5deecdf
Vidur Singh <vidursingh@Vidurs-MacBook-Pro.local>
2016-05-16 20:15:35 +0530

    third commit

--- 1stproject/ViewController.swift
+++ 1stproject/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var Supercool: UIImageView!
+    @IBOutlet weak var bg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func Button1showimage(sender: AnyObject) {
+        bg.hidden = false
+        Supercool.hidden = false
+        
+        button1.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
